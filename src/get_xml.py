a8ed6466fe501326e1c2c470930d3d74966e9a86
Matt McGuire <matt@mwmcguire.com>
2016-12-18 10:37:20 -0700

    GetXML is ready for productiion

--- src/get_xml.py
+++ src/get_xml.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from xml.etree import ElementTree as eT
 import requests
 
@@ -9,6 +10,9 @@ def well_formed_xml(response_text):
 
     If the payload is able to parsed into an ElementTree than that
     is enough to verify that the file is valid XML
+    :param response_text: content from the response, expected to be
+    an XML
+    :return: true if the content can be parsed at an XML
     """
     try:
         tree = eT.fromstring(response_text)
@@ -18,29 +22,42 @@ def well_formed_xml(response_text):
         return tree.tag
 
 
-def path_is_valid(path):
+def path_is_valid(directory):
     """Check if the path exists and is a directory
+    :param directory: the expected path passed in will be relative to
+    the project
+    :return: true if path exists and is a directory
     """
+    path = '{cwd}{dir}'.format(cwd=os.getcwd(), dir=directory)
+    if not os.path.exists(path) and not os.path.isdir(path):
+        os.makedirs(path)
     return os.path.exists(path) and os.path.isdir(path)
 
 
 class GetXML:
-    def __init__(self, urls=('http://syndication.enterprise.websiteidx'
-                             '.com/feeds/BoojCodeTest.xml',)):
-        self.urls = urls
+    """GetXML class will be used to download and store XMLs based on
+    the URLs passed in when the object is created.  By default there
+    is only one URL to download an XML from.
+    """
+
+    def __init__(self, url='http://syndication.enterprise.websiteidx'
+                           '.com/feeds/BoojCodeTest.xml'):
+        self.url = url
         self.response = requests.Response()
+        self.data_dir = '/data/'
 
     def download_xml(self, retries=2):
         """Function to download the XML file from a feed.
+        :param retries: How many times to retry downloading the file
+        :return: the status code and content of the request
 
         """
         try:
-            for url in self.urls:
-                self.response = requests.get(url)
+            self.response = requests.get(self.url)
 
-                # Attempt the download until successful
-                if self.response.status_code != 200 and retries:
-                    self.download_xml(retries - 1)
+            # Attempt the download until successful
+            if self.response.status_code != 200 and retries:
+                self.download_xml(retries - 1)
 
         except requests.ConnectionError as ce:
             print "Connection Error ({0}): {1}".format(ce.errno,
@@ -51,8 +68,8 @@ class GetXML:
             print "Timeout Error({0}): {1}".format(te.errno,
                                                    te.strerror)
         except requests.TooManyRedirects as tmr:
-            print "Too Many redirects Error({0}): {1}".format(tmr.errno,
-                                                              tmr.strerror)
+            print "Too Many redirects Error({0}): {1}".format(
+                tmr.errno, tmr.strerror)
         except requests.RequestException as re:
 
             print "General Request Error({0}): {1}".format(re.errno,
@@ -63,13 +80,27 @@ class GetXML:
                     self.response.content) \
                     else None
 
-                self.store_xml(xml)
+                file_name = self.url.split('/')[-1]
+                self.store_xml(xml, file_name)
                 return self.response.status_code, xml
             else:
                 # TODO Capture stacktrace
                 sys.exit(1)
 
-    def store_xml(self, xml, path='./data'):
-        if path_is_valid(path):
-
-            pass
+    def store_xml(self, xml, filename):
+        """Once determined that the path to store the file is valid
+        and the XML is valid than insert a millisecond-from-epoch
+        timestamp to uniquely identify each downloaded file
+        """
+        if path_is_valid(self.data_dir) and xml:
+            millis = str(int(round(time.time() * 1000)))
+            stored_filename = '{timestamp}_{name}'.format(
+                timestamp=millis,
+                name=filename)
+            file_path = '{cwd}{dir}{name}'.format(cwd=os.getcwd(),
+                                                  dir=self.data_dir,
+                                                  name=stored_filename)
+            with open(file_path, 'wt') as f:
+                f.write(xml)
+        else:
+            print 'XML downloaded was empty: {fn}'.format(fn=filename)
--- test/test_get_xml.py
+++ test/test_get_xml.py
@@ -5,35 +5,30 @@ from src.get_xml import well_formed_xml
 from src.get_xml import path_is_valid
 
 
+def clear_old_test_data():
+    """Remove all the test data from the previous test run
+    """
+    folder = '{}{}'.format(os.getcwd(), '/data_TEST/')
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)
+
+
 class TestGetXML(unittest.TestCase):
     def setUp(self):
-        self.valid_xml = """
-            <root>
-                <child>Child A</child>
-                <child>Child B</child>
-            </root>
-            """
-        self.malformed_xml = """
-            <root
-                <child>Child A</child>
-            /root>
-            """
+        self.valid_xml = "<root><child>Child A</child><child>Child " \
+                         "B</child></root> "
+        self.malformed_xml = "<root<child>Child A</child>/root>"
         self.test_dir = '/data_TEST'
         self.gX = GetXML()
-        self.badGX = GetXML(urls='mwmcguire.com')
+        self.gX.data_dir = '/data_TEST/'
 
-    def test_pos_download_xml(self):
-        """Ensure that the XML file is being downloaded as expected
-        """
-        resp_stat_code, content = self.gX.download_xml()
-        self.assertEqual(resp_stat_code, 200)
-        self.assertTrue(content)
-
-    def test_neg_download_xml(self):
-        """Ensure that when a bad URL is given that a failing
-        response code is returned and the text value is None
-        """
-        self.assertRaises(Exception, self.badGX.download_xml())
+        self.badGX = GetXML(url='mwmcguire.com')
+        self.badGX.data_dir = '/data_TEST/'
 
     def test_well_formed_xml(self):
         """Ensure that the XML is formatted validly
@@ -45,16 +40,17 @@ class TestGetXML(unittest.TestCase):
             self.malformed_xml))
 
     def test_path_is_valid(self):
-        """Ensure that the path given is valid and a directory
+        """Ensure that the path given is valid if the directory
+        exists
         """
-        self.assertFalse(path_is_valid('./fake_path'))
         self.assertTrue(path_is_valid(os.getcwd()))
 
     def test_store_xml_no_dir(self):
         """Ensure that when the directory is not present that it can
         be created and the downloaded XML can be saved to disk
         """
-        self.assertEqual(None, self.gX.store_xml(self.valid_xml))
+        self.assertEqual(None, self.gX.store_xml(self.valid_xml,
+                                                 '_TEST_FILE.xml'))
 
     def test_store_xml_dir_present(self):
         """Ensure that when the directory is already present that the
@@ -62,9 +58,22 @@ class TestGetXML(unittest.TestCase):
         """
         pass
 
-    def tearDown(self):
-        # Remove the test XML File Dir after each run
-        pass
+    def test_pos_download_xml(self):
+        """Ensure that the XML file is being downloaded as expected
+        """
+        # Clear the data from the last test run, but leave the data
+        # from this run
+        clear_old_test_data()
+        resp_stat_code, content = self.gX.download_xml()
+        self.assertEqual(resp_stat_code, 200)
+        self.assertTrue(content)
+
+    def test_neg_download_xml(self):
+        """Ensure that when a bad URL is given that a failing
+        response code is returned and the text value is None
+        """
+        self.assertRaises(Exception, self.badGX.download_xml())
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestGetXML)
 unittest.TextTestRunner(verbosity=2).run(suite)
